d67ddcb511ad49aa23b63f19923d00bb8638fe73
Joshua Martin <ursa8minority@gmail.com>
2023-04-20 21:18:45 -0600

    Create dashboard class

---
+++ src/dashBoard.js
@@ -0,0 +1,8 @@
+//http://localhost:3001/api/v1/travelers/<id>
+
+class Dashboard {
+    constructor() {
+
+    }
+}
+export default Dashboard
--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,9 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
+
+import Dashboard from './dashBoard';
+
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
